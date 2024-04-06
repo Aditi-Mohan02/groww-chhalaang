c72e6332481cae23fe00ffc174006e090671b151
s0g0cbs <sonam.gupta@walmart.com>
2024-04-06 19:10:59 +0530

    added scheduler

--- hackathon/src/main/java/com/chalang/hackathon/BackendHacathonCodeApplication.java
+++ hackathon/src/main/java/com/chalang/hackathon/BackendHacathonCodeApplication.java
@@ -2,8 +2,10 @@ package com.chalang.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendHacathonCodeApplication {
 
 	public static void main(String[] args) {
